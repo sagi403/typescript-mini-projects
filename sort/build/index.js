7d980e30d6193a727177edeb7fc7db71ccbdac08
Sagi <sagi403@gmail.com>
2022-07-29 13:57:14 +0300

    Sorting Implementation

---
+++ sort/build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+const sorted = new Sorter([10, 3, -5, 0]);
+sorted.sort();
+console.log(sorted.collection);
