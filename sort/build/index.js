31bb70fa69cabbe971a327a35ad2417c239f95e2
Sagi <sagi403@gmail.com>
2022-07-29 14:50:29 +0300

    Type guards

--- sort/build/index.js
+++ sort/build/index.js
@@ -7,10 +7,18 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+                // Only going to works if collection is number[]
+                // If collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+                // Only going to works if collection is a string
+                // If collection is a string, do this logic instead
+                if (typeof this.collection === "string") {
                 }
             }
         }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,24 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
+        // Only going to works if collection is number[]
+        // If collection is an array of numbers
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
+
+        // Only going to works if collection is a string
+        // If collection is a string, do this logic instead
+        if (typeof this.collection === "string") {
         }
       }
     }
