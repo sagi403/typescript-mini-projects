126807548fe93659bbf9ffd06e8e37affd05e5c8
Sagi <sagi403@gmail.com>
2022-07-30 13:04:23 +0300

    Sorting string implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorted = new Sorter(numbersCollection);
 sorted.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Xaayb");
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
