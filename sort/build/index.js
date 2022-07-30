615a7fc2780b57a493d37d40d727b7bfa21f4926
Sagi <sagi403@gmail.com>
2022-07-30 10:48:50 +0300

    Extracting key logic

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,29 +1,6 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                // Only going to works if collection is number[]
-                // If collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-                // Only going to works if collection is a string
-                // If collection is a string, do this logic instead
-                if (typeof this.collection === "string") {
-                }
-            }
-        }
-    }
-}
-const sorted = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const sorted = new Sorter_1.Sorter([10, 3, -5, 0]);
 sorted.sort();
 console.log(sorted.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,29 +1,4 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // Only going to works if collection is number[]
-        // If collection is an array of numbers
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = temp;
-          }
-        }
-
-        // Only going to works if collection is a string
-        // If collection is a string, do this logic instead
-        if (typeof this.collection === "string") {
-        }
-      }
-    }
-  }
-}
+import { Sorter } from "./Sorter";
 
 const sorted = new Sorter([10, 3, -5, 0]);
 sorted.sort();
