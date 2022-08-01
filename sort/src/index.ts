446052ee1a4dbc047a7fd15db5bf63342d8901a7
Sagi <sagi403@gmail.com>
2022-08-01 14:08:52 +0300

    Integrating the sort method with abstract classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,23 +1,23 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorted = new Sorter(numbersCollection);
-// sorted.sort();
-// console.log(numbersCollection.data);
+// Sort array
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection("Xaayb");
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+// Sort string
+const charactersCollection = new CharactersCollection("Xaayb");
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
+// Sort linked list
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-const sorter = new Sorter(linkedList);
-sorter.sort();
+
+linkedList.sort();
 linkedList.print();
