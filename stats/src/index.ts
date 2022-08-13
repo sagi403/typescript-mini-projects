1079c867195cc57e1b01694f179182e5c77766f8
Sagi <sagi403@gmail.com>
2022-08-13 15:15:28 +0300

    Putting it all together

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,16 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
+import { Summary } from "./Summary";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
