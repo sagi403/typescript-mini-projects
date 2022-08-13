c8efcffdbea414dbe07fe2edd4311e40d829504e
Sagi <sagi403@gmail.com>
2022-08-13 16:57:05 +0300

    Shorting the creation of the MatchReader object

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,21 +1,8 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
 import { Summary } from "./Summary";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-const csvFileReader = new CsvFileReader("football.csv");
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
-// const summary = new Summary(
-//   new WinsAnalysis("Man United"),
-//   new ConsoleReport()
-// );
-
-// const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
-
+const matchReader = MatchReader.fromCsv("football.csv");
 const summary = Summary.winsAnalysisWithHtmlReport("Man City");
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
