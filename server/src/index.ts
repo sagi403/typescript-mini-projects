114f4a662495e0dfbfa1404b8a86844e31aa33a7
Sagi <sagi403@gmail.com>
2022-09-01 14:29:25 +0300

    Wiring up sessions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { router } from "./routes/loginRoutes";
+import cookieSession from "cookie-session";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["gtrhwrgh"] }));
 app.use(router);
 
 app.listen(3000, () => {
