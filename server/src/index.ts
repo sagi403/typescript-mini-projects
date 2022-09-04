3c69187408e8793693737ad7fa6dd60fb9dcf2c9
Sagi <sagi403@gmail.com>
2022-09-04 18:14:28 +0300

    Fixing routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
-import { router } from "./routes/loginRoutes";
 import cookieSession from "cookie-session";
 import { AppRouter } from "./AppRouter";
 import "./controllers/LoginController";
+import "./controllers/RootController";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["gtrhwrgh"] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
