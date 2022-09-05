7a82cca15e20ef847260197c1b6293827265537b
Sagi <sagi403@gmail.com>
2022-09-05 14:10:10 +0300

    Action creator with typescript

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -1,7 +1,15 @@
-import axios from "axios"
+import axios from "axios";
+import { Dispatch } from "redux";
+
+const url = "https://jsonplaceholder.typicode.com/todos";
 
 export const fetchTodos = () => {
-  return (dispatch: ) => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get(url);
 
-  }
-}
+    dispatch({
+      type: "FETCH_TODOS",
+      payload: response.data,
+    });
+  };
+};
