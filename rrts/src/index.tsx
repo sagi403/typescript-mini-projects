c25b111a706a4a77e08a24981c3545247cac9c7a
Sagi <sagi403@gmail.com>
2022-09-05 11:16:14 +0300

    Handling component state

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-class App extends React.Component {
+interface AppProps {
+  color: string;
+}
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>Hi there</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
@@ -11,5 +31,5 @@ const container = document.getElementById("root");
 
 if (container) {
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(<App color="red" />);
 }
